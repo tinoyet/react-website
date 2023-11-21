94b4ee6655d193effb7884f58330c74a94e1b1d4
Tinoyet <406269693@qq.com>
2023-11-21 23:38:44 +0800

    feat: head3

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,16 +4,43 @@ import heroImg from '@/assets/img/hero-image.png';
 import icon_01 from '@/assets/img/icon/icon-01.png';
 import icon_02 from '@/assets/img/icon/icon-02.png';
 import footerListLineImg from '@/assets/img/footer-list-line.png';
-import bottom1Img from '@/assets/img/bottom-1.png';
-import bottom2Img from '@/assets/img/bottom-2.png';
-import bottom3Img from '@/assets/img/bottom-3.png';
-import bottom4Img from '@/assets/img/bottom-4.png';
+import bottom1Img from '@/assets/img/icon/icon-03.png';
+import bottom2Img from '@/assets/img/icon/icon-04.png';
+import bottom3Img from '@/assets/img/icon/icon-05.png';
+import bottom4Img from '@/assets/img/icon/icon-06.png';
+import icon_07 from '@/assets/img/icon/icon-07.png';
+import icon_08 from '@/assets/img/icon/icon-08.png';
+import icon_09 from '@/assets/img/icon/icon-09.png';
+import icon_10 from '@/assets/img/icon/icon-10.png';
+import icon_11 from '@/assets/img/icon/icon-11.png';
 export default function index() {
   const { t } = useTranslation();
-
+  const jumpTo = (url: any) => {
+    const w: any = window.open('_black');
+    w.location.href = url;
+  };
   return (
     <div className="home w-full color-fff">
-      <div className="home-top" style={{ backgroundImage: `url(${heroImg})` }}>
+      <div className="home-top relative" style={{ backgroundImage: `url(${heroImg})` }}>
+        <div className="right-btn flex-start-center">
+          <div className="font-600">{t('Follow us on')}</div>
+          <div className="mx-15 w-40 h-1 bg-color-fff"></div>
+          <div className="mr-20 w-20 h-20 icon-image">
+            <Image src={icon_07} preview={false} height={20} width={20}></Image>
+          </div>
+          <div className="mr-20 w-20 h-20 icon-image">
+            <Image src={icon_08} preview={false} height={20} width={20}></Image>
+          </div>
+          <div className="mr-20 w-20 h-20 icon-image">
+            <Image src={icon_09} preview={false} height={20} width={20}></Image>
+          </div>
+          <div className="mr-20 w-20 h-20 icon-image">
+            <Image src={icon_10} preview={false} height={20} width={20}></Image>
+          </div>
+          <div className="mr-20 w-20 h-20 icon-image">
+            <Image src={icon_11} preview={false} height={20} width={20}></Image>
+          </div>
+        </div>
         <div className="w-1460 mx-auto pt-325">
           <div className="w-770">
             <div className="text-60 l-60 pl-25">{t('PLAY')}</div>
@@ -31,22 +58,31 @@ export default function index() {
           </div>
           <div className="flex-start-center pl-20">
             <Image src={icon_01} preview={false} height={220} width={263}></Image>
-            <Image className="ml-20" src={icon_02} preview={false} height={92} width={388}></Image>
+            <Image
+              className="ml-20 cursor-pointer"
+              src={icon_02}
+              preview={false}
+              height={92}
+              width={388}
+              onClick={() => {
+                jumpTo('https://youtu.be/py6R8wYQDAA');
+              }}
+            ></Image>
           </div>
-          <div>
+          <div className="pl-25">
             <div className="text-14">{t('Available on')}:</div>
-            <div className="flex-start-center">
+            <div className="flex-start-center mt-35">
               <div className="mr-36px flex-center">
-                <Image src={bottom1Img} width={95} preview={false}></Image>
+                <Image src={bottom1Img} width={120} preview={false}></Image>
               </div>
               <div className="mr-36px flex-center">
-                <Image src={bottom2Img} width={122} preview={false} className="mr-36px"></Image>
+                <Image src={bottom2Img} width={155} preview={false} className="mr-36px"></Image>
               </div>
               <div className="mr-36px flex-center">
-                <Image src={bottom3Img} width={92} preview={false} className="mr-36px"></Image>
+                <Image src={bottom3Img} width={116} preview={false} className="mr-36px"></Image>
               </div>
               <div className=" flex-center">
-                <Image src={bottom4Img} width={95} preview={false}></Image>
+                <Image src={bottom4Img} width={121} preview={false}></Image>
               </div>
             </div>
           </div>
