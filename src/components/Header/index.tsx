360a14afc8120bf02a74d57ae671e0e783bf41cb
tinoyet <406269693@qq.com>
2023-11-24 18:38:21 +0800

    feat: edit config file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ import buyNowSignImg from '@/assets/img/buy-now-sign.png';
 import languageImg from '@/assets/img/language.png';
 import downImg from '@/assets/img/down.png';
 export default function index() {
-  const [active, setActive] = useState(0);
+  const [active, setActive] = useState('GAME PACK');
   const [selectLang, setSelectLang] = useState('1');
   const items: MenuProps['items'] = [
     {
@@ -21,6 +21,10 @@ export default function index() {
     },
   ];
 
+  const tabs: string[] = ['GAME PACK', 'MEDIA', 'NEWS', 'COMMUNITY', 'SUPPORT'];
+
+  const tabsHandle = (active: string) => setActive(active);
+
   return (
     <div className="header h-135px w-full min-w-1680 max-w-1850 mx-auto flex items-center justify-between relative px-34px box-border z-2">
       <div className="flex items-center">
@@ -28,66 +32,18 @@ export default function index() {
           <Image src={logoImg} width={'100%'} preview={false}></Image>
         </div>
         <div className="h-full flex text-12px color-white ml-78px pb-27px">
-          <div
-            className={`w-7.6vw max-w-146px min-w-100px h-108px flex-center relative cursor-pointer ${
-              active === 0 && 'nav-active'
-            }`}
-          >
-            <span>GAME PACK</span>
-            {active === 0 && (
-              <div className="absolute bottom--67px left-0 h-142px w-full">
-                <Image src={navActiveImg} width={'100%'} height={142} preview={false}></Image>
-              </div>
-            )}
-          </div>
-          <div
-            className={`w-7.6vw max-w-146px min-w-100px h-108px flex-center relative  cursor-pointer ${
-              active === 1 && 'nav-active'
-            }`}
-          >
-            <span>MEDIA</span>
-            {active === 1 && (
-              <div className="absolute bottom--67px left-0 h-142px w-full">
-                <Image src={navActiveImg} width={'100%'} height={142} preview={false}></Image>
-              </div>
-            )}
-          </div>
-          <div
-            className={`w-7.6vw max-w-146px min-w-100px h-108px flex-center relative  cursor-pointer ${
-              active === 2 && 'nav-active'
-            }`}
-          >
-            <span>NEWS</span>
-            {active === 2 && (
-              <div className="absolute bottom--67px left-0 h-142px w-full">
-                <Image src={navActiveImg} width={'100%'} height={142} preview={false}></Image>
-              </div>
-            )}
-          </div>
-          <div
-            className={`w-7.6vw max-w-146px min-w-100px h-108px flex-center relative  cursor-pointer ${
-              active === 3 && 'nav-active'
-            }`}
-          >
-            <span>COMMUNITY</span>
-            {active === 3 && (
-              <div className="absolute bottom--67px left-0 h-142px w-full">
-                <Image src={navActiveImg} width={'100%'} height={142} preview={false}></Image>
-              </div>
-            )}
-          </div>
-          <div
-            className={`w-7.6vw max-w-146px min-w-100px h-108px flex-center relative  cursor-pointer ${
-              active === 4 && 'nav-active'
-            }`}
-          >
-            <span>SUPPORT</span>
-            {active === 4 && (
-              <div className="absolute bottom--67px left-0 h-142px w-full">
-                <Image src={navActiveImg} width={'100%'} height={142} preview={false}></Image>
-              </div>
-            )}
-          </div>
+          {tabs.map((item: any) => (
+            <div
+              key={item}
+              className={`w-7.6vw max-w-146px min-w-100px h-108px flex-center relative cursor-pointer`}
+              onClick={() => tabsHandle(item)}
+            >
+              <span className={` flex-center h-50px hover ${active == item ? 'nav-cur color-00CFFF' : ''}`}>{item}</span>
+              {active == item && (
+                <img src={navActiveImg} width={120} className="absolute bottom--67px left-0 h-142px w-full" alt="" />
+              )}
+            </div>
+          ))}
         </div>
       </div>
       <div className="flex pb-27px">
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   plugins: [
     unocss(),
     react(),
-    eslintPlugin(),
+
     Icons({
       compiler: 'jsx',
       jsx: 'react',
@@ -77,7 +77,7 @@ export default defineConfig({
   server: {
     host: true, // host设置为true才可以使用network的形式，以ip访问项目
     port: 8080, // 端口号
-    open: true, // 自动打开浏览器
+    open: false, // 自动打开浏览器
     cors: true, // 跨域设置允许
     strictPort: true, // 如果端口已占用直接退出
   },
